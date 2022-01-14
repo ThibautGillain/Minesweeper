eb570ffa3ebb5c54514173dbbae7db8ce1f832f9
ThibautGillain <thibaut.gillain@insa-lyon.fr>
2022-01-14 12:49:13 +0100

    commented and cleaned code

--- src/Minesweeper.hs
+++ src/Minesweeper.hs
@@ -4,8 +4,17 @@ import qualified Data.Set as Set
 import System.Random
 import Data.Maybe
 
+-- This module is modelling the game of Minesweeper
+
 type Cell = (Int, Int)
 
+-- The Board type models the entire board of the Minesweeper game. The parameters represents the following :
+-- width, height : Size of the grid
+-- allCells : Set of all cells of the grid
+-- untouchedCells : Set of the cells that are not discovered yet. Same as allCells at start
+-- discoveredCells : Set of the cells that have been discovered. Empty at start
+-- flaggedCells : Set of the cells that have been flagged by the player. Empty at start
+-- bombs : Set of cells which contains bombs. Generated randomly when generating a board.
 data Board = Board { 
     width :: Int,
     height :: Int,
@@ -16,6 +25,9 @@ data Board = Board {
     bombs :: Set.Set Cell
 } deriving (Show)
 
+
+
+-- Endgame detection functions. Using Sets to model the current state of the board makes those functions quite straightforward.
 isBoardLost :: Board -> Bool
 isBoardLost board = not $ null $ (bombs board)  `Set.intersection` (discoveredCells board)
 
@@ -26,6 +38,9 @@ isBoardWon board = (not (isBoardLost board)) && (sizeOfUnion == getBoardSize boa
 isGameEnded :: Board -> Bool
 isGameEnded board = isBoardLost board || isBoardWon board
 
+
+
+-- Functions to update the board (insert/delete cells in sets) and getters
 discoverCell :: Board -> Cell -> Board
 discoverCell board cell = if (not $ isDiscovered board cell) && (not $ isFlagged board cell)
                           then board { 
@@ -35,18 +50,12 @@ discoverCell board cell = if (not $ isDiscovered board cell) && (not $ isFlagged
 
 flagCell :: Board -> Cell -> Board
 flagCell board cell = if not $ isDiscovered board cell 
-                    --   then board { 
-                    --     flaggedCells = Set.insert cell (flaggedCells board) 
-                    --     , untouchedCells = Set.delete cell (untouchedCells board)}
                       then board { 
                         flaggedCells = Set.insert cell (flaggedCells board)}
                       else board
 
 unflagCell :: Board -> Cell -> Board
-unflagCell board cell = if (isFlagged board cell) && (not $ isDiscovered board cell) 
-                        -- then board { 
-                        --     flaggedCells = Set.delete cell (flaggedCells board)
-                        --     , untouchedCells = Set.insert cell (untouchedCells board) }
+unflagCell board cell = if (isFlagged board cell) && (not $ isDiscovered board cell)
                         then board { 
                             flaggedCells = Set.delete cell (flaggedCells board)}
                         else board
@@ -72,30 +81,31 @@ isBomb board cell = Set.member cell (bombs board)
 getBoardSize :: Board -> Int
 getBoardSize board = (width board) * (height board)
 
+
+
+-- Functions to get the neighbours of a cell
 getNeighbourCells :: Cell -> Int -> Int -> Set.Set Cell
-getNeighbourCells cell width height = Set.fromList $ filter (cellInBounds width height) (getTopNeighbours cell ++ getSideNeighbours cell ++ getBottomNeighbours cell)
+getNeighbourCells cell width height = Set.fromList $ filter (cellInBounds width height) (getNeighbours cell)
 
-getTopNeighbours :: Cell -> [Cell]
-getTopNeighbours cell = [(rowIndex-1, columnIndex-1), (rowIndex-1, columnIndex), (rowIndex-1, columnIndex+1)]
+getNeighbours :: Cell -> [Cell]
+getNeighbours cell = [(rowIndex-1, columnIndex-1), (rowIndex-1, columnIndex), (rowIndex-1, columnIndex+1),
+                         (rowIndex, columnIndex-1)                             , (rowIndex, columnIndex+1),
+                         (rowIndex+1, columnIndex-1), (rowIndex+1, columnIndex), (rowIndex+1, columnIndex+1)]
                         where rowIndex = fst cell
                               columnIndex = snd cell
 
-getBottomNeighbours :: Cell -> [Cell]
-getBottomNeighbours cell = [(rowIndex+1, columnIndex-1), (rowIndex+1, columnIndex), (rowIndex+1, columnIndex+1)]
-                            where rowIndex = fst cell
-                                  columnIndex = snd cell
-
-getSideNeighbours :: Cell -> [Cell]
-getSideNeighbours cell = [(rowIndex, columnIndex-1), (rowIndex, columnIndex+1)]
-                        where rowIndex = fst cell
-                              columnIndex = snd cell
-
+-- This function returns the number of bombs that are in the neighbourhood of a cell.
 countNeighbouringBombs :: Board -> Cell -> Int
 countNeighbouringBombs board cell = Set.size $ Set.intersection (bombs board) (getNeighbourCells cell (width board) (height board))
 
+-- This is an utility function to keep only the cells that are really in the grid (for example a cell in the corner only has 3 neighbours and not 8)
+-- The ranges are the following : [1,height] and [1, width]
 cellInBounds :: Int -> Int -> Cell -> Bool
 cellInBounds width height (row, column) = row > 0 && row <= height && column > 0 && column <= width
 
+
+
+-- This functions generate all the cells of a board given his size.
 generateCellsSet :: Int -> Int -> Set.Set Cell
 generateCellsSet width height = Set.fromList $ generateCellsList rowIndexes columnIndexes
                                 where rowIndexes = [1..height]
@@ -108,10 +118,10 @@ generateCellsList rowIndexes columnIndexes = generateCellsRow (head rowIndexes) 
 generateCellsRow :: Int -> [Int] -> [Cell]
 generateCellsRow rowIndex columnIndexes = (zip (replicate (length columnIndexes) rowIndex) columnIndexes)
 
--- TODO: To be done randomly
--- generateBombs :: Int -> Int -> Int -> Set.Set Cell
--- generateBombs height width n = do return Set.empty
 
+-- These functions are randomly generating mines and then fills these mines into the corresponding set in the board
+-- We first generate a board with an empty bomb set and we generate and fill the bombs separately because we have to 
+-- use the IO monad for the random generation.
 generateBombsList :: Int -> Int -> Int -> IO [Cell]
 generateBombsList _ _ 0 = return []
 generateBombsList height width n = do
@@ -143,8 +153,81 @@ generateBoardWithBombs w h n = do
     return $ generateMinesInBoard board bombsList
 
 
--- https://github.com/endymion64/HaskellMines/blob/master/MyBoard.hs
-                                    
+
+-- These three functions are used to play an automatic obvious move if possible. This move is the simplest one, it is
+-- just returning a cell which is an undiscovered neighbour of a cell which has no bombs in its neighbourhood.
+-- We are using the Maybe monad to either return a Just Cell or Nothing if there are no safe move available in the current board.
+-- This could have be done automatically by discovering all the cells which have 0 neighbours when clicking one of those cells but I didn't manage to achieve that.
+safeMove :: Board -> Maybe Cell
+safeMove board = getCellWithoutBombInNeighbours board
+
+getCellWithoutBombInNeighbours :: Board -> Maybe Cell
+getCellWithoutBombInNeighbours board = if (Set.size cellsWithoutBombsInNeighbours /= 0)
+                                                 then Set.lookupGT (0,0) $ getUntouchedNeighbours board (Set.lookupGT (0,0) cellsWithoutBombsInNeighbours)
+                                                 else Nothing
+                                                 where cellsWithoutBombsInNeighbours = Set.filter (hasNoNeighbouringBombs board) (discoveredCells board)
+
+hasNoNeighbouringBombs :: Board -> Cell -> Bool
+hasNoNeighbouringBombs board cell = if (countNeighbouringBombs board cell == 0 && getNumberOfUntouchedNeighbours board cell /= 0)
+                                    then True
+                                    else False
+
+
+
+-- These three functions are used to play an automatic obvious move if possible. This is a move that will flag a cell if a discovered cell in the board
+-- has as much untouched neighbours as neighbouring mines. It will then return one of the neighbour cell as a cell to flag.
+-- It is the simplest pattern described here http://www.minesweeper.info/wiki/Strategy
+safeMoveToFlag :: Board -> Maybe Cell
+safeMoveToFlag board = getObviousCellToFlag board
+
+getObviousCellToFlag :: Board -> Maybe Cell
+getObviousCellToFlag board = if (Set.size cellsWithSameBombsAndUntouchedInNeighbours /= 0)
+                                then Set.lookupGT (0,0) $ getUntouchedAndUnflaggedNeighbours board (Set.lookupGT (0,0) cellsWithSameBombsAndUntouchedInNeighbours)
+                                else Nothing
+                                where cellsWithSameBombsAndUntouchedInNeighbours = Set.filter (hasSameNeighbouringBombsAndUntouched board) (discoveredCells board)
+
+hasSameNeighbouringBombsAndUntouched :: Board -> Cell -> Bool
+hasSameNeighbouringBombsAndUntouched board cell = if (numberOfNeighbouringBombs == numberOfUntouchedNeighbours && numberOfFlaggedNeighbours < numberOfNeighbouringBombs)
+                                                    then True
+                                                    else False
+                                                  where numberOfNeighbouringBombs = countNeighbouringBombs board cell
+                                                        numberOfUntouchedNeighbours = getNumberOfUntouchedNeighbours board cell
+                                                        numberOfFlaggedNeighbours = getNumberOfFlaggedNeighbours board cell
+
+
+
+-- These functions are utility functions used by the automatic move functions
+getNumberOfUntouchedNeighbours :: Board -> Cell -> Int
+getNumberOfUntouchedNeighbours board cell = Set.size $ Set.intersection neighbours (untouchedCells board)
+                                            where neighbours = getNeighbourCells cell (width board) (height board)
+
+getNumberOfFlaggedNeighbours :: Board -> Cell -> Int
+getNumberOfFlaggedNeighbours board cell = Set.size $ Set.intersection neighbours (flaggedCells board)
+                                          where neighbours = getNeighbourCells cell (width board) (height board)
+
+getUntouchedNeighbours :: Board -> Maybe Cell -> Set.Set Cell
+getUntouchedNeighbours board cell = Set.intersection (untouchedCells board) (getNeighbourCells (fromJust cell) (width board) (height board))
+
+getUntouchedAndUnflaggedNeighbours :: Board -> Maybe Cell -> Set.Set Cell
+getUntouchedAndUnflaggedNeighbours board cell = Set.filter (isUnflagged board) (Set.intersection (untouchedCells board) (getNeighbourCells (fromJust cell) (width board) (height board)))
+
+
+
+-- These functions are returning a String to format the cell displaying depending on what they contain
+bombCell :: String
+bombCell = "B"
+
+flaggedCell :: String
+flaggedCell = "F"
+
+untouchedCell :: String
+untouchedCell = "."
+
+
+
+-- These functions were used at the beginning as a tool to test that the modelisation of the game was well designed.
+-- It is a simple displaying of the board in the console without any interaction.
+-- It has been mainly inspired by this code on github: https://github.com/endymion64/HaskellMines/blob/master/MyBoard.hs                              
 showBoard :: Board -> String
 showBoard board = 
     printBorder (width board) ++
@@ -177,51 +260,6 @@ printCells currentRow currentColumn board =
     in currentCellFormat ++ printCells currentRow (currentColumn -1) board
 
 
-safeMove :: Board -> Maybe Cell
-safeMove board = getCellWithoutBombInNeighbours board
-
-safeMoveToFlag :: Board -> Maybe Cell
-safeMoveToFlag board = getObviousCellToFlag board
-
-getCellWithoutBombInNeighbours :: Board -> Maybe Cell
-getCellWithoutBombInNeighbours board = if (Set.size cellsWithoutBombsInNeighbours /= 0)
-                                                 then Set.lookupGT (0,0) $ getUntouchedNeighbours board (Set.lookupGT (0,0) cellsWithoutBombsInNeighbours)
-                                                 else Nothing
-                                                 where cellsWithoutBombsInNeighbours = Set.filter (hasNoNeighbouringBombs board) (discoveredCells board)
-
-getObviousCellToFlag :: Board -> Maybe Cell
-getObviousCellToFlag board = if (Set.size cellsWithSameBombsAndUntouchedInNeighbours /= 0)
-                                then Set.lookupGT (0,0) $ getUntouchedAndUnflaggedNeighbours board (Set.lookupGT (0,0) cellsWithSameBombsAndUntouchedInNeighbours)
-                                else Nothing
-                                where cellsWithSameBombsAndUntouchedInNeighbours = Set.filter (hasSameNeighbouringBombsAndUntouched board) (discoveredCells board)
-
-hasNoNeighbouringBombs :: Board -> Cell -> Bool
-hasNoNeighbouringBombs board cell = if (countNeighbouringBombs board cell == 0 && getNumberOfUntouchedNeighbours board cell /= 0)
-                                    then True
-                                    else False
-
-hasSameNeighbouringBombsAndUntouched :: Board -> Cell -> Bool
-hasSameNeighbouringBombsAndUntouched board cell = if (numberOfNeighbouringBombs == numberOfUntouchedNeighbours && numberOfFlaggedNeighbours < numberOfNeighbouringBombs)
-                                                    then True
-                                                    else False
-                                                  where numberOfNeighbouringBombs = countNeighbouringBombs board cell
-                                                        numberOfUntouchedNeighbours = getNumberOfUntouchedNeighbours board cell
-                                                        numberOfFlaggedNeighbours = getNumberOfFlaggedNeighbours board cell
-
-getNumberOfUntouchedNeighbours :: Board -> Cell -> Int
-getNumberOfUntouchedNeighbours board cell = Set.size $ Set.intersection neighbours (untouchedCells board)
-    where neighbours = getNeighbourCells cell (width board) (height board)
-
-getNumberOfFlaggedNeighbours :: Board -> Cell -> Int
-getNumberOfFlaggedNeighbours board cell = Set.size $ Set.intersection neighbours (flaggedCells board)
-    where neighbours = getNeighbourCells cell (width board) (height board)
-
-getUntouchedNeighbours :: Board -> Maybe Cell -> Set.Set Cell
-getUntouchedNeighbours board cell = Set.intersection (untouchedCells board) (getNeighbourCells (fromJust cell) (width board) (height board))
-
-getUntouchedAndUnflaggedNeighbours :: Board -> Maybe Cell -> Set.Set Cell
-getUntouchedAndUnflaggedNeighbours board cell = Set.filter (isUnflagged board) (Set.intersection (untouchedCells board) (getNeighbourCells (fromJust cell) (width board) (height board)))
-
 bombCellConsole :: String
 bombCellConsole = "| B "
 
@@ -230,12 +268,3 @@ flaggedCellConsole = "| F "
 
 untouchedCellConsole :: String
 untouchedCellConsole = "| . "
-
-bombCell :: String
-bombCell = "B"
-
-flaggedCell :: String
-flaggedCell = "F"
-
-untouchedCell :: String
-untouchedCell = "."
--- src/UI.hs
+++ src/UI.hs
@@ -10,53 +10,77 @@ import Control.Monad.Trans (liftIO)
 
 import Minesweeper
 
+-- This module implements the User Interface of the Minesweeper game.
+-- It uses the Threepenny GUI library.
+-- The User Interface is pretty simple.
+
+-- Size of the canvas containing the board
 canvasSize :: Int
 canvasSize = 25*boardSize
 
+-- Size of the board. It's stored as a single constant so we only have square grids but it can be splitted into two
+-- different constants to play with rectangular grids
 boardSize :: Int
 boardSize = 5
 
+-- Density of mines in the board. It can be increased to add difficulty.
 density :: Double
 density = 0.15
 
+-- This data type represents the main possible actions that a user can do on a Minesweeper Board
 data Modes = Discover | Flag | UnFlag
 
 startMinesweeperUI :: IO ()
 startMinesweeperUI = do
     startGUI defaultConfig setup
 
+
+-- This setup method sets up the HTML page displayed to the user. 
+-- It also manages every possible interactions with the user inside the UI monad. It uses IORef to maintain some kind of state
 setup :: Window -> UI ()
 setup w = do
     return w # set UI.title "Minesweeper"
 
+    -- Setting of the canvas 
     canvas <- UI.canvas
         # set UI.height canvasSize
         # set UI.width canvasSize
         # set UI.style [("border", "solid black 1px"), ("background", "#eee")]
 
+    -- Generation of the initial board
     initialBoard <- liftIO $ generateBoardWithBombs boardSize boardSize (floor $ (fromIntegral (boardSize*boardSize)) * density)
     
+    -- State of the application. Three IORefs represent the current board, the current mode (Discover, Flag, Unflag) and the current mouse position in the canvas.
     currentBoard <- liftIO $ newIORef initialBoard
     mode <- liftIO $ newIORef Discover
     pos <- liftIO $ newIORef (0,0)
 
+    -- Different buttons of the UI
     discoverMode <- UI.button #+ [string "DISCOVER"]
     flagMode <- UI.button #+ [string "FLAG"]
     unflagMode <- UI.button #+ [string "UNFLAG"]
     newBoardButton <- UI.button #+ [string "NEW BOARD"]
     playMoveButton <- UI.button #+ [string "PLAY MOVE"]
 
+    -- Those are two div containers and different messages that are used to display the end game message, the current mode and a message
+    -- when we use the "PLAY MOVE" button but the application couldn't find an obvious move.
     currentModeContainer <- UI.div
     currentModeString <- string "Current mode : DISCOVER"
     element currentModeContainer # set children [currentModeString]  
 
     endGameMessageContainer <- UI.div
-    noSafeMoveMessageContainer <- UI.div
     winMessage <- string "You successfully uncovered all the safe cells. Click NEW BOARD to play another game"
     lostMessage <- string "You lost, you hit a mine. Click NEW BOARD to play another game "
+
+    noSafeMoveMessageContainer <- UI.div
     noSafeMoveMessage <- string "No obvious safe move available"
+
+    -- We draw the board in the canvas.
     drawBoard initialBoard False canvas
 
+    -- On click functions:
+
+    -- The first three functions binds to the mode buttons the update of the IORef representing the current mode of play.
     on UI.click discoverMode $ \_ -> 
         do liftIO $ writeIORef mode Discover
            currentModeString <- string "Current mode : DISCOVER"
@@ -72,6 +96,9 @@ setup w = do
            currentModeString <- string "Current mode : UNFLAG"
            element currentModeContainer # set children [currentModeString] 
 
+    -- This function binds to the "NEW BOARD" button the generation of a new board.
+    -- It generates a new board and then writes it in the IORef representing the current board. It also sets the mode back to Discover.
+    -- It finally draw the new board in the canvas.
     on UI.click newBoardButton $ \_ ->
         do newBoard <- liftIO $ generateBoardWithBombs boardSize boardSize (floor $ (fromIntegral (boardSize*boardSize)) * density)
            element noSafeMoveMessageContainer # set children []
@@ -82,6 +109,10 @@ setup w = do
            element endGameMessageContainer # set children []
            drawBoard newBoard False canvas
 
+    -- This function binds to the "PLAY MOVE" button the automatic play of a play if there's an obvious play available.
+    -- It first checks for a safe play to discover a cell that is definitely not containing a bomb.
+    -- If there's no such play available. It will check if there is a cell that definitely contains a bomb and it will flag it.
+    -- If those two moves are not available, it will just display to the user that no obvious move are currently available.
     on UI.click playMoveButton $ \_ ->
         do current <- liftIO $ readIORef currentBoard
            case safeMove current of
@@ -103,9 +134,16 @@ setup w = do
                             element noSafeMoveMessageContainer # set children [noSafeMoveMessage]
                             return ()
 
+    -- This method binds to the movement of the mouse in the canvas the update of the pos IORef which contains 
+    -- the position of the mouse at any time.
     on UI.mousemove canvas $ \(x,y) ->
         do liftIO $ writeIORef pos (x,y)
 
+    -- This method binds a click in the canvas to the corresponding play.
+    -- It will first check if the game is not ended and that the board should be clickable.
+    -- If the user can play, il will converts the mouse position to the actual cell that the user clicked
+    -- and play the move accordingly to the current mode.
+    -- In the end, it will update the current board IORef and display the new board.
     on UI.mousedown canvas $ \_ -> do
         (x,y) <- liftIO $ readIORef pos
         m <- liftIO $ readIORef mode
@@ -150,7 +188,7 @@ setup w = do
                     liftIO $ writeIORef currentBoard newBoard
                     drawBoard newBoard False canvas
     
-
+    -- We add to the body element all the elements of our page.
     getBody w #+ [
         column [element canvas]
         , element discoverMode
@@ -163,6 +201,9 @@ setup w = do
         , element endGameMessageContainer]
     return ()
 
+
+-- These functions are drawing the board on the canvas.
+-- It first draw the lines of the grid and then draw each of the cells of the board using recursion.
 drawBoard :: Board -> Bool -> Element  -> UI ()
 drawBoard board showBombs canvas = do
     canvas # UI.clearCanvas
@@ -177,7 +218,6 @@ drawRow :: Int -> Board -> Bool -> Element -> UI ()
 drawRow 0 _ _ _ = return ()
 drawRow currentRow board showBombs canvas = do drawCells currentRow (width board) board showBombs canvas
                                                drawRow (currentRow - 1) board showBombs canvas
-
 
 drawCells :: Int -> Int -> Board -> Bool -> Element -> UI ()
 drawCells _ 0 _ _ _ = return ()
@@ -217,42 +257,17 @@ drawLine canvas (a, b) = do
     UI.closePath canvas
     UI.stroke canvas
 
-getCellIndexFromMousePos :: (Int, Int) -> (Int, Int)
-getCellIndexFromMousePos (x, y) = 
-    ((floor $ (fromIntegral x)/25.0)+1, (floor $ (fromIntegral y)/25.0)+1) 
-
+-- This function returns a few colours depending on the format of the cell to draw in the canvas
 getCellColour :: String -> String
 getCellColour "B" = "red"
+getCellColour "F" = "black"
 getCellColour "0" = "grey"
 getCellColour "1" = "blue"
 getCellColour "2" = "green"
 getCellColour "3" = "red"
 getCellColour _ = "brown"
 
--- setup :: Window -> UI ()
--- setup w = do
---     displayBoard w board
-
--- displayBoard :: Window -> Board -> UI ()
--- displayBoard w board = do displayRow w (height board) board
-
--- displayRow :: Window -> Int -> Board -> UI ()
--- displayRow w 0 board = do getBody w #+ [string "\n"]
---                           return ()
--- displayRow w currentRow board = do getBody w #+ [row $ displayCells w currentRow (width board) board]
---                                    displayRow w (currentRow - 1) board
-
--- displayCells :: Window -> Int -> Int -> Board -> [UI Element]
--- displayCells w _ 0 _ = []
--- displayCells w currentRow currentColumn board =
---     let x = (width board) - currentColumn + 1
---         y = (height board) - currentRow + 1
---         currentCell = (x, y)
---         currentCellFormat = if (isBomb board currentCell)
---                             then bombCell
---                             else if (isFlagged board currentCell)
---                                  then flaggedCell
---                                  else if (isUntouched board currentCell)
---                                       then untouchedCell
---                                       else show $ countNeighbouringBombs board currentCell
---     in [UI.button # set text currentCellFormat # set UI.style [("width", "50px")]] ++ displayCells w currentRow (currentColumn - 1) board
+-- This function converts the mouse position to the corresponding cell indexes.
+getCellIndexFromMousePos :: (Int, Int) -> (Int, Int)
+getCellIndexFromMousePos (x, y) = 
+    ((floor $ (fromIntegral x)/25.0)+1, (floor $ (fromIntegral y)/25.0)+1) 
